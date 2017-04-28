96bf6e91ccd978fb73a4c80dbeaa9bb46964f06d
Richard Barnes <rbarnes@umn.edu>
2017-04-28 01:19:30 -0700

    Added tests. Adapted for parallelism.

--- cpp_attempt.cpp
+++ cpp_attempt.cpp
@@ -9,6 +9,11 @@
 #include <stdexcept>
 #include <algorithm>
 #include <array>
+#include <iomanip>
+#include <cassert>
+
+const double IEL = std::atan(0.5); //Icosahedron equatorial latitudes
+const double IES = 36*M_PI/180;    //Icosahedron equatorial spacing
 
 template<class T>
 void ToRadians(T &vec){
@@ -27,6 +32,16 @@ class LineString {
   std::vector<double> x;
   std::vector<double> y;
 };
+
+const char* wgs84_str = "+init=epsg:4326"; // EPSG:4326 definition: http://spatialreference.org/ref/epsg/4326/proj4/
+const char* merc_str  = "+init=epsg:3857";
+const projPJ pj_wgs84 = pj_init_plus(wgs84_str);
+const projPJ pj_merc  = pj_init_plus(merc_str);
+
+template<class T>
+void ToMercator(T &x, T &y){
+  pj_transform(pj_wgs84, pj_merc, x.size(), 1, x.data(), y.data(), NULL);
+}
 
 void LatLonToXYZ(
   const double lat,
@@ -55,9 +70,9 @@ void XYZtoLatLon(
 
 
 void RotatePoint(const double rlat, const double rlon, const double rtheta, double &lat, double &lon){
-  lon += M_PI+rtheta;                          //Move [0,360] and add theta
+  lon += M_PI+rtheta;                                    //Move [0,360] and add theta
   lon  = std::fmod(2*M_PI+std::fmod(lon,2*M_PI),2*M_PI); //Map back to [0,360]
-  lon -= M_PI;                                 //Move back to [-180,180] system
+  lon -= M_PI;                                           //Move back to [-180,180] system
 
   double xr, yr, zr;
   LatLonToXYZ(lat, lon, 1, xr, yr, zr);
@@ -66,6 +81,73 @@ void RotatePoint(const double rlat, const double rlon, const double rtheta, doub
   double z = -std::sin(rlat)*xr + std::cos(rlat)*zr;
   XYZtoLatLon(x,y,z, lat, lon);
 }
+
+//Info: https://wrf.ecse.rpi.edu//Research/Short_Notes/pnpoly.html
+//Info: https://stackoverflow.com/questions/8721406/how-to-determine-if-a-point-is-inside-a-2d-convex-polygon/23223947#23223947
+//Info: http://stackoverflow.com/a/2922778/752843
+int ContainsPoint(const std::vector<double> &vertx, const std::vector<double> &verty, const double testx, const double testy) {
+  unsigned int i, j;
+  int c = 0;
+  for (i = 0, j = vertx.size()-1; i < vertx.size(); j = i++) {
+    if ( ((verty[i]>testy) != (verty[j]>testy)) &&
+     (testx < (vertx[j]-vertx[i]) * (testy-verty[i]) / (verty[j]-verty[i]) + vertx[i]) )
+       c = !c;
+  }
+  return c;
+}
+
+// """
+// Calculate the Great Circle distance on Earth between two latitude-longitude
+// points
+// :param lat1 Latitude of Point 1 in degrees
+// :param lon1 Longtiude of Point 1 in degrees
+// :param lat2 Latitude of Point 2 in degrees
+// :param lon2 Longtiude of Point 2 in degrees
+// :returns Distance between the two points in kilometres
+// """
+double Haversine(
+  const double lon1, 
+  const double lat1, 
+  const double lon2,
+  const double lat2 
+){
+  const double Rearth = 6371; //km
+  const double dlon   = lon2 - lon1;
+  const double dlat   = lat2 - lat1;
+  const double a      = std::pow(std::sin(dlat/2),2) + std::cos(lat1) * std::cos(lat2) * std::pow(std::sin(dlon/2),2);
+  const double c      = 2*std::asin(std::sqrt(a));
+  return Rearth*c;
+}
+
+
+const double vla = 26.57;
+
+class Pole {
+ public:
+  std::array<double,12> lon = {{     0,      0,-5*IES,-4*IES,-3*IES, -2*IES,-1*IES,   0, 1*IES,  2*IES,3*IES,4*IES}};
+  std::array<double,12> lat = {{M_PI/2,-M_PI/2,   IEL,  -IEL,   IEL,   -IEL,   IEL,-IEL,   IEL,   -IEL,  IEL, -IEL}};
+
+  Pole(){}
+
+  void rotatePole(double rlat, double rlon, double rtheta){
+    for(unsigned int i=0;i<lon.size();i++)
+      RotatePoint(rlat, rlon, rtheta, lat[i], lon[i]);
+  }
+
+  void toMercator(){
+    ToMercator(lon,lat);
+  }
+
+  void print() const {
+    auto templon = lon;
+    auto templat = lat;
+    ToDegrees(templon);
+    ToDegrees(templat);
+    for(unsigned int i=0;i<lon.size();i++)
+      std::cout<<std::setw(10)<<templat[i]<<" "<<std::setw(10)<<templon[i]<<std::endl;
+  }
+};
+
 
 class Polygon {
  public:
@@ -95,29 +177,37 @@ class Polygon {
     for(auto &i: interior.x) i *= RAD_TO_DEG;
     for(auto &i: interior.y) i *= RAD_TO_DEG;
   }
+  bool containsPoint(const double testx, const double testy) const {
+    return ContainsPoint(exterior.x, exterior.y, testx, testy);
+  }
+
+  // """
+  // Calculate the closest distance between a polygon and a latitude-longitude
+  // point, using only spherical considerations. Ignore edges.
+  // :param lat  Latitude of query point in degrees
+  // :param lon  Longitude of query point in degrees
+  // :param geom A `shapely` geometry whose points are in latitude-longitude space
+  // :returns: The minimum distance in kilometres between the polygon and the
+  //           query point
+  // """
+  double distanceFromPoint(const double px, const double py) const {
+    double dist = std::numeric_limits<double>::infinity();
+    for(unsigned int i=0;i<exterior.x.size();i++)
+      dist = std::min(dist,Haversine(px,py,exterior.x[i],exterior.y[i]));
+    return dist;
+  }
+
+  double distanceFromPole(const Pole &p) const {
+    double dist = std::numeric_limits<double>::infinity();
+    for(unsigned int i=0;i<p.lon.size();i++)
+      dist = std::min(dist,distanceFromPoint(p.lon[i],p.lat[i]));
+    return dist;
+  }
 };
 
 
 
-const double vla = 26.57;
 
-class Pole {
- public:
-  std::array<double,12> plon = {{ 90,0,-180,-144,-108, -72,-36,   0, 36,  72,108, 144}};
-  std::array<double,12> plat = {{-90,0, vla,-vla, vla,-vla,vla,-vla,vla,-vla,vla,-vla}};
-  Pole(){
-    ToRadians(plon);
-    ToRadians(plat);
-  }
-
-  void rotatePole(double rlat, double rlon, double rtheta){
-    rlat *= DEG_TO_RAD;
-    rlon *= DEG_TO_RAD;
-
-    for(int i=0;i<plon.size();i++)
-      RotatePoint(rlat, rlon, rtheta, plat[i], plon[i]);
-  }
-};
 
 void ReadShapefile(std::string filename, std::string layername, std::vector<Polygon> &geometries){
   GDALAllRegister();
@@ -169,6 +259,7 @@ void ReadShapefile(std::string filename, std::string layername, std::vector<Poly
   GDALClose( poDS );
 }
 
+//Saving to disk: https://stackoverflow.com/questions/33395445/libspatialindex-loading-storing-index-on-disk
 class SpIndex {
  private:
   Index* idx;
@@ -200,21 +291,18 @@ class SpIndex {
 
   void addBox(double left, double bottom, double right, double top, int64_t id){
     //Create array with lat/lon points
-    double low[]    = {left,bottom};
-    double high[]   = {right,top};
+    double low[]  = {left,bottom};
+    double high[] = {right,top};
 
     //Shapes can also have an object associated with them, but not this one!
     uint8_t* pData = 0;
     uint32_t nDataLength = 0;
 
     // create shape
-    SpatialIndex::IShape* shape = new SpatialIndex::Region(low,high,2);
+    auto shape = SpatialIndex::Region(low,high,2);
 
     // insert into index along with the an object and an ID
-    idx->index().insertData(nDataLength,pData,*shape,id);
-
-    delete shape;
-
+    idx->index().insertData(nDataLength,pData,shape,id);
   }
 
   void addPolygon(const Polygon &p, int64_t id){
@@ -225,7 +313,7 @@ class SpIndex {
     addBox(left,bottom,right,top, id);
   }
 
-  std::vector<int64_t> overlaps(double x, double y, double maxResults){
+  std::vector<int64_t> overlaps(double x, double y) const {
     double coords[] = {x,y};
 
     //Object that will show us what results we've found
@@ -250,27 +338,121 @@ class SpIndex {
 };
 
 
+int CountOverlaps(const Pole &p, const SpIndex &sp, const std::vector<Polygon> &polygons){
+  int overlaps = 0;
+  for(unsigned int i=0;i<p.lat.size();i++){
+    auto sp_overlaps = sp.overlaps(p.lon[i],p.lat[i]);
+    if(sp_overlaps.size()==0)
+      continue;
+    for(auto &pi: sp_overlaps){
+      if(polygons[pi].containsPoint(p.lon[i],p.lat[i])){
+        overlaps++;
+        break;
+      }
+    }
+  }
+  return overlaps;
+}
 
 
+void Test(){
+  std::cerr<<"Running tests..."<<std::endl;
 
-int main(int argc, char **argv){
-  std::vector<Polygon> geometries;
-  ReadShapefile("data/simplified-land-polygons-complete-3857/simplified_land_polygons.shp", "simplified_land_polygons", geometries);
-  //ReadShapefile("data/land-polygons-split-3857/land_polygons.shp", "land_polygons", geometries);
-  std::cerr<<"Read "<<geometries.size()<<" polygons."<<std::endl;
+  {
+    Pole p;
+    p.rotatePole(23*DEG_TO_RAD,44*DEG_TO_RAD,5*DEG_TO_RAD);
+    p.print();
+  }
+
+  SpIndex sp;
+  for(double y=0;y<10;y++)
+  for(double x=0;x<10;x++)
+    sp.addBox(x,y,x+1,y+1,y*10+x);
+
+  assert(sp.overlaps(3.5,3.5)[0]==33);
+  assert(sp.overlaps(7.5,5.5)[0]==57);
+
+  Polygon p;
+  p.exterior.x.push_back(70); p.exterior.y.push_back(70);
+  p.exterior.x.push_back(70); p.exterior.y.push_back(80);
+  p.exterior.x.push_back(80); p.exterior.y.push_back(80);
+  p.exterior.x.push_back(80); p.exterior.y.push_back(70);
+
+  sp.addPolygon(p, 347);
+
+  assert(sp.overlaps(75,77)[0]==347);
+
+  assert(p.containsPoint(75,77));
+  assert(p.containsPoint(74.3,72.2));
+  assert(!p.containsPoint(69.4,72.2));
+
+  std::cerr<<"Passed"<<std::endl;
+}
+
+struct POI {
+  int overlaps;
+  double rlat;
+  double rlon;
+  double rtheta;
+  double distance;
+};
+
+std::vector<struct POI> FindPolesOfInterest(){
+  std::vector<Polygon> landmass_merc;
+  std::cerr<<"Reading Mercator split shapefile..."<<std::endl;
+  ReadShapefile("data/simplified-land-polygons-complete-3857/simplified_land_polygons.shp", "simplified_land_polygons", landmass_merc);
+  //ReadShapefile("data/land-polygons-split-3857/land_polygons.shp", "land_polygons", landmass_merc);
+  std::cerr<<"Read "<<landmass_merc.size()<<" polygons."<<std::endl;
 
   SpIndex sp;
 
   std::cerr<<"Building index..."<<std::endl;
-  for(int64_t i=0;i<geometries.size();i++)
-    sp.addPolygon(geometries[i], i);
+  for(int64_t i=0;(unsigned)i<landmass_merc.size();i++) //TODO: Fix this unsigned int64 mess
+    sp.addPolygon(landmass_merc[i], i);
   std::cerr<<"Index built."<<std::endl;
 
-  for(auto &i: sp.overlaps(-11501094,3771603,5000))
-    std::cerr<<"Found "<<i<<std::endl;
+  std::cerr<<"Finding poles..."<<std::endl;
+  std::vector<struct POI> pois;
+  #pragma omp parallel for collapse(3)
+  for(int plat  =0; plat  <634; plat+=1  ) //(pi()/2-vla)*180/pi()
+  for(int plon  =0; plon  <720; plon+=1  )
+  for(int ptheta=0; ptheta<720; ptheta+=1){
+    Pole p;
+    p.rotatePole(plat/10.0*DEG_TO_RAD, plon/10.0*DEG_TO_RAD, ptheta/10.0*DEG_TO_RAD);
+    p.toMercator();
+    auto overlaps = CountOverlaps(p,sp,landmass_merc);
+    if(overlaps==0 || overlaps>=8){
+      #pragma omp critical
+      pois.push_back(POI{overlaps,plat/10.0,plon/10.0,ptheta/10.0,-1});
+    }
+  }
 
-  Pole p;
-  p.rotatePole(23*DEG_TO_RAD,44*DEG_TO_RAD,5*DEG_TO_RAD);
+  std::cerr<<"Found "<<pois.size()<<" poles of interest."<<std::endl;
+
+  return pois;
+}
+
+void DistancesToPoles(std::vector<struct POI> &pois){
+  std::cerr<<"Reading WGS84 shapefile..."<<std::endl;
+  std::vector<Polygon> landmass_wgs84;
+  ReadShapefile("data/land-polygons-complete-4326/land_polygons.shp", "land_polygons", landmass_wgs84);
+
+  std::cerr<<"Calculating distances to poles..."<<std::endl;
+  for(auto &poi: pois){
+    Pole p;
+    p.rotatePole(poi.rlat*DEG_TO_RAD, poi.rlon*DEG_TO_RAD, poi.rtheta*DEG_TO_RAD);
+    poi.distance = std::numeric_limits<double>::infinity();
+    for(const auto &g: landmass_wgs84)
+      poi.distance = std::min(poi.distance, g.distanceFromPole(p));
+  }
+}
+
+int main(int argc, char **argv){
+  Test();
+
+  auto pois = FindPolesOfInterest();
+
+  //DistancesToPoles(pois);
 }
 
 
@@ -294,21 +476,9 @@ int main(int argc, char **argv){
 
 
 
+
 /*
-  void toMercator(){
-    // EPSG:4326 definition: http://spatialreference.org/ref/epsg/4326/proj4/
-    const char* wgs84 = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs";
-    // EPSG:27700 definition: http://spatialreference.org/ref/epsg/27700/proj4/
-    const char* merc = "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs";
-    
-    projPJ pj_src = pj_init_plus(src);
-    projPJ pj_dst = pj_init_plus(dst);
 
-    toRadian();
-
-    pj_transform(wgs84, merc, exterior.x.size(), 1, exterior.x.data(), exterior.y.data(), NULL);
-    pj_transform(wgs84, merc, interior.x.size(), 1, interior.x.data(), interior.y.data(), NULL);
-  }
   void toWGS84(){
     // EPSG:4326 definition: http://spatialreference.org/ref/epsg/4326/proj4/
     const char* wgs84 = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs";
