fbc82f4df612a589bee6f9c45464be5dcdfcdbd7
Richard Barnes <rbarnes@umn.edu>
2017-05-04 22:23:52 -0700

    Initial import

---
+++ SpIndex.cpp
@@ -0,0 +1,33 @@
+#include "SpIndex.hpp"
+#include <CGAL/Cartesian.h>
+#include <CGAL/Segment_tree_k.h>
+#include <CGAL/Range_segment_tree_traits.h>
+
+SpIndex::SpIndex(){
+  
+}
+
+void SpIndex::addBox(
+  const int xmin, 
+  const int ymin, 
+  const int xmax, 
+  const int ymax, 
+  const int id
+){
+  intervals_to_add.push_back(Interval(Pure_interval(Key(xmin,ymin), Key(xmax,ymax)),id));
+}
+
+void SpIndex::buildIndex(){
+  index = Segment_tree_2_type(intervals_to_add.begin(),intervals_to_add.end());
+  intervals_to_add.clear();
+}
+
+int SpIndex::queryPoint(const int px, const int py) {
+  std::list<Interval> output;
+  const auto q = Interval(Pure_interval(Key(px,py), Key(px+1,py+1)),-2);
+  index.window_query(q,std::back_inserter(output));
+  if(output.size()==0)
+    return -1;
+  else
+    return output.front().second;
+}
---
+++ SpIndex.hpp
@@ -0,0 +1,28 @@
+#ifndef _SpIndex_hpp_
+#define _SpIndex_hpp_
+
+#include <CGAL/Cartesian.h>
+#include <CGAL/Segment_tree_k.h>
+#include <CGAL/Range_segment_tree_traits.h>
+#include <list>
+
+class SpIndex {
+ private:
+  typedef CGAL::Cartesian<int> K;
+  typedef CGAL::Segment_tree_map_traits_2<K, int> Traits;
+  typedef CGAL::Segment_tree_2<Traits> Segment_tree_2_type;
+  typedef Traits::Interval Interval;
+  typedef Traits::Pure_interval Pure_interval;
+  typedef Traits::Key Key;
+  typedef std::list<Interval> IList;
+  Segment_tree_2_type index;
+  IList intervals_to_add;
+
+ public:
+  SpIndex();
+  void addBox(const int xmin, const int ymin, const int xmax, const int ymax, const int id);
+  void buildIndex();
+  int queryPoint(const int px, const int py);
+};
+
+#endif
