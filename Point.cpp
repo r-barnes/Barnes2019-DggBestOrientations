673c22655dd87d86874d3ea3d82b686b8029101e
Richard Barnes <rbarnes@umn.edu>
2017-08-01 15:23:24 -0500

    Formatting

--- Point.cpp
+++ Point.cpp
@@ -164,11 +164,11 @@ TEST_CASE("Point2D: Conversion to Degrees"){
 
 
 Point2D& Point2D::rotateTheta(const double rtheta) {
-  x += M_PI+rtheta;                                  //Move [0,360] and add theta
-  x = std::fmod(x,2*M_PI);                           //Map back to [0,360]
+  x += M_PI+rtheta;        //Move [0,360] and add theta
+  x = std::fmod(x,2*M_PI); //Map back to [0,360]
   if(x<0)
     x += 2*M_PI;
-  x -= M_PI;                                         //Move back to [-180,180] system
+  x -= M_PI;               //Move back to [-180,180] system
   return *this;
 }
 
